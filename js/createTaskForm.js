d16ea4c02f578cde549beb366c752c18a192f708
RudovichNazar23 <n.rudovich2005@gmail.com>
2023-09-23 16:00:02 +0200

    edited check_date_finish file

--- js/createTaskForm.js
+++ js/createTaskForm.js
@@ -1,26 +1,5 @@
 "use strict";
 
-function checkDivError(){
-    let err_divs = document.body.querySelectorAll("div[id=error]");
-    for (let div of err_divs){
-        if(div.classList.contains("invalid")){
-            return true;
-        }
-    }
-    return false;
-}
-
-function checkAllformFields(event){
-    let inputs = Array.from(document.body.querySelectorAll("input"));
-    inputs.push(document.querySelector("textarea"));
-    let button = document.body.querySelector("button[type=submit]");
-    let empty_fields = inputs.filter((node) => !node.value);
-
-    if (empty_fields.length >= 1 || checkDivError()){
-        button.classList.add("disabled");
-    }
-    else{ button.classList.remove("disabled");}
-};
 
 function checkFieldIsEmpty(event){
     let input = event.target;
@@ -54,3 +33,34 @@ function getFormData(event){
     let taskObject = createTaskObject(title, content, date_start, date_finish, reminder, category, priority);
     addToLocalStorage(taskObject);
 };
+
+
+class FormHandler{
+    handleEvent(){
+        this.checkAllformFields();
+    }
+    checkDivError(){
+        let err_divs = document.body.querySelectorAll("div[id=error]");
+        for (let div of err_divs){
+            if(div.classList.contains("invalid")){
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    checkAllformFields(){
+        let inputs = Array.from(document.body.querySelectorAll("input"));
+        inputs.push(document.querySelector("textarea"));
+        let button = document.body.querySelector("button[type=submit]");
+        let empty_fields = inputs.filter((node) => !node.value);
+    
+        if (empty_fields.length >= 1 || this.checkDivError()){
+            button.classList.add("disabled");
+        }
+        else{ button.classList.remove("disabled");}
+    };
+}
+
+let form = document.body.querySelector("form");
+form.addEventListener("mouseover", new FormHandler());

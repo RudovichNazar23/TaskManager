9a74c5f0d8389f3df688b72a48260628591fb372
RudovichNazar23 <n.rudovich2005@gmail.com>
2023-09-23 14:17:20 +0200

    added new form checker

--- js/createTaskForm.js
+++ js/createTaskForm.js
@@ -1,13 +1,22 @@
 "use strict";
 
-// Add form validators
+function checkDivError(){
+    let err_divs = document.body.querySelectorAll("div[id=error]");
+    for (let div of err_divs){
+        if(div.classList.contains("invalid")){
+            return true;
+        }
+    }
+    return false;
+}
+
 function checkAllformFields(event){
     let inputs = Array.from(document.body.querySelectorAll("input"));
     inputs.push(document.querySelector("textarea"));
     let button = document.body.querySelector("button[type=submit]");
     let empty_fields = inputs.filter((node) => !node.value);
 
-    if (empty_fields.length >= 1){
+    if (empty_fields.length >= 1 || checkDivError()){
         button.classList.add("disabled");
     }
     else{ button.classList.remove("disabled");}
